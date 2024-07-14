6a3ff61b4f862fccc278262def1823e46f606db9
sangshrestha <sang.socials@gmail.com>
2024-07-14 22:47:11 +0100

    test: ignore utility function tests

--- utility.test.js
+++ utility.test.js
@@ -1,6 +1,6 @@
 import { isLetter, isDigitString } from "./utility";
 
-describe("isLetter()", () => {
+xdescribe("isLetter()", () => {
   it("returns true for allowed chars", () => {
     const allowedChars = [
       "A",
@@ -87,7 +87,7 @@ describe("isLetter()", () => {
   });
 });
 
-describe("isDigitString()", () => {
+xdescribe("isDigitString()", () => {
   it("returns true for string digits", () => {
     const allowedDigits = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
     allowedDigits.forEach((digit) => {
