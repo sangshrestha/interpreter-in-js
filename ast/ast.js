9a0fa3e11e3c546dd9e34caf7c90609e6c1d5e53
sangshrestha <sang.socials@gmail.com>
2024-07-29 21:50:49 +0100

    fix: initialise block string with empty string

--- ast/ast.js
+++ ast/ast.js
@@ -154,7 +154,7 @@ export function IfExpression(token, condition, consequence, alternative) {
 
 export function BlockStatement(token, statements) {
   function string() {
-    let blockString;
+    let blockString = "";
 
     for (const statement of statements) {
       blockString += statement.string();
